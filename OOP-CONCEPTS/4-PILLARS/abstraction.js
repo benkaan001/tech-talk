10ab94341197fd0db5da32fd5c465eefe5051dd0
ben kaan <benkaan001@gmail.com>
2022-03-04 01:04:17 -0600

    add OOP concepts

---
+++ OOP-CONCEPTS/4-PILLARS/abstraction.js
@@ -0,0 +1,12 @@
+/*
+
+Abstraction means that the user interacts with only selected
+properties and methods of an object.
+
+Abstraction is an extension of encapsulation. For example, the
+user doesn't need to know what is inside the car engine to be
+able to drive the car.
+
+This way we protect the data from exposure.
+
+*/
---
+++ OOP-CONCEPTS/4-PILLARS/encapsulation.js
@@ -0,0 +1,12 @@
+/*
+Encapsulation requires to define what properties and methods are private and
+what methods/properties are public. For example, a car's engine is private
+while its blinkers are public.
+
+Encapsulation adds SECURITY. Properties and methods can be made private,
+so they are not accessible outside the class.
+
+Encapsulation hides COMPLEXITY. No one can see what's behind the object's curtain!
+
+In JS, protected fields are prefixed with a _
+*/
